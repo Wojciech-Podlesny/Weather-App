304a4754b3c0e17ecbce667b51acf507238eccd2
Wojciech Podleśny <wojciech.podlesny@outlook.com>
2024-07-11 11:24:33 +0200

    Add component ErrorToast

---
+++ src/components/ErrorToast/ErrorToast.tsx
@@ -0,0 +1,22 @@
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+type ErrorToastProps = {
+  message: string
+}
+
+const ErrorToast = ({ message }: ErrorToastProps) => {
+  toast(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  })
+
+  return <ToastContainer />
+}
+
+export { ErrorToast }
--- src/components/LocationSearch/LocationSearch.tsx
+++ src/components/LocationSearch/LocationSearch.tsx
@@ -3,23 +3,21 @@ import { useWeather } from '../../hooks/useWeather'
 import { FaSearch } from 'react-icons/fa'
 import { LuLocateFixed } from 'react-icons/lu'
 import { LocationSearchProps } from '../../types/LocationSearch'
+import { ToastContainer } from 'react-toastify'
+import { ErrorToast } from '../ErrorToast/ErrorToast'
 
 const LocationSearch = ({ isMobileSearchOpen, onSearchClose, theme }: LocationSearchProps) => {
 	const [city, setCity] = useState<string>('')
 	const { setLocation } = useWeather()
-	const [error, setError] = useState<string>('')
 	const [isFetching, setIsFetching] = useState<boolean>(false)
 
 	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setCity(e.target.value)
-		if (error) {
-			setError('')
-		}
 	}
 
 	const handleSearch = () => {
 		if (city.trim() === '') {
-			setError('Location cannot be empty')
+			showErrorToast('Location cannot be empty')
 			return
 		}
 
@@ -47,22 +45,28 @@ const LocationSearch = ({ isMobileSearchOpen, onSearchClose, theme }: LocationSe
 				error => {
 					console.error('Error fetching geolocation:', error)
 					setIsFetching(false)
+					showErrorToast('Error fetching geolocation')
 				}
 			)
 		} else {
 			console.error('Geolocation is not supported by this browser.')
 			setIsFetching(false)
+			showErrorToast('Geolocation is not supported by this browser.')
 		}
 	}
 
-	const iconColor = theme === 'dark' ? 'black' : 'black';
+	const showErrorToast = (message: string) => {
+		ErrorToast({ message })
+	}
+
+	const iconColor = theme === 'dark' ? 'black' : 'black'
 
 	return (
 		<div
 			className={`relative items-center rounded-md px-3 py-2 ${
 				isMobileSearchOpen ? 'block' : 'hidden'
-			}  lg:block w-full max-w-md`}
-		>
+			} lg:block w-full max-w-md`}>
+			<ToastContainer />
 			<button onClick={handleSearch} className='absolute left-8 top-1/2 transform -translate-y-1/2'>
 				<FaSearch color={iconColor} />
 			</button>
@@ -75,14 +79,11 @@ const LocationSearch = ({ isMobileSearchOpen, onSearchClose, theme }: LocationSe
 					placeholder='Search location'
 					className='placeholder-gray-500 pr-10 pl-16 w-full py-2 text-base rounded-2xl border-2 border-black'
 				/>
-				{error && <p className='text-red-500 mt-1'>{error}</p>}
 			</div>
-
 			<button
 				onClick={fetchGeolocation}
 				disabled={isFetching}
-				className='absolute right-6 top-1/2 transform -translate-y-1/2'
-			>
+				className='absolute right-6 top-1/2 transform -translate-y-1/2'>
 				<LuLocateFixed size={'1.5em'} color={iconColor} />
 			</button>
 		</div>
